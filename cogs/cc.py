1283562280fd5633348ccc801f9ee36343bc014f
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-08-24 22:36:00 +0800

    test

--- cogs/cc.py
+++ cogs/cc.py
@@ -22,5 +22,10 @@ class cog:
     em = discord.Embed(author="4JR",image="https://discordapp.com/api/v6/users/180314310298304512/avatars/eb45214491b879d0db62a8165148a311.jpg",title="Echo Help",description="Visit https://echo.xtclabs.net/ to learn about the basic things Echo can do \nVisit https://ars.xtclabs.net/ to learn about the ever-evolving ARS. \nVisit https://github.com/proxikal/Echo to see Echo 1.0 Documentation!", color=0x00FFFF)
     await self.bot.say(embed=em)
     
+  @commands.command(pass_context=True)
+  async def help(self):
+    em = discord.Embed(author="4JR",image="https://discordapp.com/api/v6/users/180314310298304512/avatars/eb45214491b879d0db62a8165148a311.jpg",title="Help",description="Some Cool Text!", color=0x00FFFF)
+    await self.bot.say(embed=em)
+    
 def setup(bot):
   bot.add_cog(cog(bot))
