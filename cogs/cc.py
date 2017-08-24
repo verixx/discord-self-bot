227e1d566e596f15921374aa7cf64e4dc3d2197f
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-08-24 20:07:18 +0800

    added import

--- cogs/cc.py
+++ cogs/cc.py
@@ -1,3 +1,11 @@
+import discord
+from discord.ext import commands
+import datetime
+import time
+import random
+import asyncio
+import json
+
 @commands.command(pass_context=True)
 async def test(self, ctx): 
   await bot.say('hello')
