7d556d8a337f1355ab5be669183acb1a873dd0da
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-08-24 20:34:49 +0800

    rename cog

--- cogs/cc.py
+++ cogs/cc.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands
 
-class YourCog:
+class Custom Commands:
   def __init__(self, bot):
     self.bot = bot
 
   @commands.command()
-  async def test(self):
+  async def echo-help(self):
+    await self.bot.say('hi')
+    
+  @commands.command()
+  async def hi(self):
     await self.bot.say('hello')
-
+    
 def setup(bot):
-  bot.add_cog(YourCog(bot))
+  bot.add_cog(Custom Commands(bot))
