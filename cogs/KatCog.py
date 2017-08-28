1f44adcea8f055d1866a89163484114dec24f86e
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-08-28 16:26:39 -0500

    Update KatCog.py

--- cogs/KatCog.py
+++ cogs/KatCog.py
@@ -20,13 +20,11 @@ class KatCog:
     '''Post certain memes'''
     em=discord.Embed(color=0xed, description='meme')
     list=discord.Embed(title='Memes:', description='Testing\n Testing #2', color=0xed)
-    if 'testing' in message:
       em.set_image(url="https://images-ext-1.discordapp.net/external/I2njA8Ftmb2rA34vLWK__QVBgH8v4qCh-ZNfIBZ9PUA/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/300396755193954306/a73725eaa41bf59a73d283f35b280781.png?width=96&height=96") # An avatar link to demonstrate
-
-    await self.bot.say(embed=em)
-      else:
-          if 'list' in message:
-            await self.bot.say(embed=list)
+    if 'testing' in message:
+      await self.bot.say(embed=em)
+    if 'list' in message:
+      await self.bot.say(embed=list)
     
   @commands.command(pass_context = True)
   async def slap(self,ctx, *, person: str):
