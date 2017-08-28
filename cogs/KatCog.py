efe7ffc77a5872c742064c4898314c517376708d
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-08-27 21:18:54 -0500

    Add files via upload

---
+++ cogs/KatCog.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+class KatCog:
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.command(pass_context = True)
+  async def hi(self, ctx):
+    await self.bot.say('Hello!!')
+    await self.bot.delete_message('Hello!!')
+
+  @commands.command(pass_context = True)
+  async def copycat(self, ctx, *, message: str):
+    '''Copy what you just said'''
+    await self.bot.say(message)
+
+  @commands.command(pass_context = True)
+  async def meme(self, ctx, *, meme : str=None):
+    '''Post certain memes'''
+    em=discord.Embed(colour=discord.Colour(value=int(("#%06x" % random.randint(8, 0xFFFFFF)),16)), description=meme)
+    if 'testing' in meme:
+      em.set_image(url="https://images-ext-1.discordapp.net/external/I2njA8Ftmb2rA34vLWK__QVBgH8v4qCh-ZNfIBZ9PUA/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/300396755193954306/a73725eaa41bf59a73d283f35b280781.png?width=96&height=96") # An avatar link to demonstrate
+
+    await self.bot.say(embed=em)
+    
+  @commands.command(pass_context = True)
+  async def slap(self,ctx, *, person: str):
+      name = ctx.message.author
+      embed=discord.Embed(color=0xed, title="{} has slapped {}".format(name.name, person))
+      embed.set_image(url="https://i.ytimg.com/vi/7AXB8nGq5jc/maxresdefault.jpg")
+      await self.bot.say(embed=embed)
+      await self.bot.delete_message(ctx.message)
+    
+def setup(bot):
+  bot.add_cog(KatCog(bot))
