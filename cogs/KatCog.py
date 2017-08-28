ae6cd490d6f0904df983b4e3b02db1e16f9c56bf
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-08-28 17:01:14 -0500

    Update KatCog.py

--- cogs/KatCog.py
+++ cogs/KatCog.py
@@ -22,7 +22,7 @@ class KatCog:
     list=discord.Embed(title='Memes:', description='Doge\nFeelsBadMan', color=0xed)
     doge.set_image(url="http://i0.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg")
     pepe=discord.Embed(color=0xed, description='FeelsBadMan'
-    pepe.set_image(url="https://m.imgur.com/gallery/aSVjtu7")
+    pepe.set_image(url="https://cdn.nplus1.ru/images/2016/12/30/d238c7d3f81f8246281e52f5d377e48d.jpg")
     if 'doge' in message:
       await self.bot.say(embed=doge)
     if 'list' in message:
