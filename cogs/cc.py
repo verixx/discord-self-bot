5661862ef7abc479b46b843d4aa36fa2234ab311
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-04 00:10:22 +0800

    fix shit

--- cogs/cc.py
+++ cogs/cc.py
@@ -21,20 +21,26 @@ class cog:
   @commands.command(pass_context=True)
   async def echohelp(self):
     em = discord.Embed(title="Echo Help",description="Visit https://echo.xtclabs.net/ to learn about the basic things Echo can do \nVisit https://ars.xtclabs.net/ to learn about the ever-evolving ARS. \nVisit https://github.com/proxikal/Echo to see Echo 1.0 Documentation!", color=0x00FFFF)
-    em.set_author(name = "4JR", icon_url = "https://discordapp.com/api/v6/users/180314310298304512/avatars/eb45214491b879d0db62a8165148a311.jpg")
+    em.set_author(name = "4JR",  icon_url = "https://discordapp.com/api/v6/users/180314310298304512/avatars/eb45214491b879d0db62a8165148a311.jpg")
     await self.bot.say(embed=em)
     
   @commands.command() 
   async def status(self, *, text):
     if text == 'online':
        await bot.change_presence(status=discord.Status.online, afk=True)
+       await bot.say('Set your status to online')
     if text == 'idle':
        await bot.change_presence(status=discord.Status.idle, afk=True)
+       await bot.say('Set your status to idle')
     if text == 'dnd':
        await bot.change_presence(status=discord.Status.dnd, afk=True)
-    if text == 'invisible':
+       await bot.say('Set your status to DND')
+    if text == 'invis':
        await bot.change_presence(status=discord.Status.invisible, afk=True)
- 
+       await bot.say('Set your status to invisible')
+
+    
+
     
   #async def on_message(self, message):
     #if message.author != self.bot.user:
