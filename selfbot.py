8fc651947e3a1b6a3af41d6081d77e9a93d0f9d4
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-05 09:48:17 +0800

    Add files via upload

--- selfbot.py
+++ selfbot.py
@@ -76,6 +76,8 @@ async def on_ready():
           'User ID: {}\n'
           '------------------------------------------'
     	  .format(bot.user, bot.user.id))
+    await bot.change_presence(status=discord.Status.invisible, afk=True)
+ 
 
 @bot.command(pass_context=True)
 async def ping(ctx):
